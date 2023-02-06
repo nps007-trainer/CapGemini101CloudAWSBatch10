c368b4b234239d97f48898d3b71217207186867e
nps007-trainer <gl.dombivali@gmail.com>
2023-02-06 20:12:41 +0530

    Mockito, Lombok, Swagger

--- SpringBootServiceLayered/src/main/java/com/capg/springboot/SpringBootServiceLayeredApplication.java
+++ SpringBootServiceLayered/src/main/java/com/capg/springboot/SpringBootServiceLayeredApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
+// Enabling swagger documentation and UI
 @SpringBootApplication
 @EnableSwagger2
 public class SpringBootServiceLayeredApplication {
